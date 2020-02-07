4bed1d5fe67eddca9ee636e719c7dabf30504f11
rilmarramos <rilmarramoslastra@gmail.com>
2020-02-07 16:57:39 -0300

    mejorade nhe :v

--- rilmar/src/App.js
+++ rilmar/src/App.js
@@ -7,16 +7,27 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        
+      <form>
+        <label className="nombre">
+          Nombre : <input type="text" />
+        
+        </label>
+         
+         <label className="apellido">
+          Apellido: <input type="text"/>
+         
+         </label>
+         
+         </form>
+        
+         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
       </header>
     </div>
